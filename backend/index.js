850f90ae22cfa6a9fa9e81cc2db1bb8f544193e5
DarreDevyll <daran.parkman@gmail.com>
2023-08-10 11:24:48 -0500

    added connection to local database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,58 @@
 const express = require("express");
+
+const bodyParser = require('body-parser');
+const mariadb = require("mariadb");
+
 const app = express();
 const port = 3000;
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Still don't know what this does
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Conrol-Allow-Headers", "Origin,\
+     X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+const pool = mariadb.createPool({
+    host: '127.0.0.1',
+    user: 'darre',
+    password: 'tempPass',
+    database: 'testBoard',
+    connectionLimit: 5
+})
+
+/* An Export I copied. idk what imma do with it yet. don't fully understand exports/middlewares in express yet
+module.exports = function(req, res, next) {
+    const message = {};
+    message.body = req.responseObject;
+    message.success = true;
+    message.status = req.responseStatus || 200;
+    res.status(message.status).send(message);
+    return next();
+}*/
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    pool.getConnection()
+    .then((conn) => {
+        conn.query("SELECT * FROM prayers;")
+        .then((rows) => {
+            res.send(rows);
+            conn.end();
+        })
+        .catch(err => {
+            console.log(err);
+            conn.end();
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
 
 app.listen(port, () => {
